f57f16836b67829b8dcd55f1cf49df7694a5f3a8
Robert Shoemate <robert@dronesense.com>
2017-02-27 23:17:43 -0600

    Added an example of using javascript to change styles.

---
+++ CSS Styles/Example 4 - JavaScript/scripts/main.js
@@ -0,0 +1,9 @@
+function changeStyleToSectionHeading1() {
+    var sectionHeading = document.getElementById('sectionHeading');
+    sectionHeading.className = "sectionHeadingStyle1";
+}
+
+function changeStyleToSectionHeading2() {
+    var sectionHeading = document.getElementById('sectionHeading');
+    sectionHeading.className = "sectionHeadingStyle2";
+}
